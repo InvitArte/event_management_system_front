73d4f7fc1711e6107ef682ae4754f7f59414af9e
pmunbe <pmunoz@aztechnology.es>
2024-07-30 10:51:38 +0200

    Solucion carga de imagenes y actualizacion vista publica (germinar)

--- src/components/PublicView/Collaboration.jsx
+++ src/components/PublicView/Collaboration.jsx
@@ -1,11 +1,36 @@
+// src/components/PublicView/Collaboration.jsx
+import React, { useEffect } from "react";
 import logo from "../../assets/imgs/logo_eslogan.png";
 import sello from "../../assets/imgs/sello.png";
 import "../../styles/PublicView/Collaboration.css";
+import { useBackgroundImage } from "../../context/BackgroundImageContext";
+
 const Collaboration = () => {
+  const { backgroundImages, setBackgroundImage } = useBackgroundImage();
+
+  useEffect(() => {
+    const loadBackgroundImage = async () => {
+      try {
+        // Cargar la imagen de fondo de manera dinamica
+        const imageModule = await import("../../assets/imgs/collaboration.jpg");
+        setBackgroundImage("collaboration", imageModule.default);
+      } catch (error) {
+        console.error(
+          "Error loading background image:",
+          error.message || error
+        );
+      }
+    };
+    loadBackgroundImage();
+  }, [setBackgroundImage]);
+
   return (
-    <div className="collaboration">
+    <div
+      className="collaboration"
+      style={{ backgroundImage: `url(${backgroundImages.collaboration})` }}
+    >
       <div className="collaboration-overlay">
-        <h3>EN COLARORACIÓN CON</h3>
+        <h3>EN COLABORACIÓN CON</h3>
         <img src={logo} alt="Logo" className="collaboration-logo" />
         <img src={sello} alt="Sello" className="collaboration-sello" />
       </div>
