6db90ae6dfc11ba52046e8cfe1c226695b09adfb
Ángel Caparrós Caballero <135209076+kubaliski@users.noreply.github.com>
2024-10-17 22:45:30 +0200

    feat(guests/tags): Refactor Excel Logic, Add Dynamic Column Sizes, and Implement Tag Filters
    - Refactorizado ExcelDownloader en un componente genérico.
    - Movida la lógica de los datos de cada Excel a los hooks personalizados.
    - Ajustado el tamaño de las columnas para que sea dinámico.
    - Creada la lógica de TagFilter en TagView.

--- src/views/TagView/TagViewComponents/index.js
+++ src/views/TagView/TagViewComponents/index.js
@@ -3,3 +3,4 @@ export {default as TagForm} from "./TagForm";
 export {default as TagModal} from "./TagModal";
 export {default as TagTable} from "./TagTable";
 export {default as MobileTagList} from "./MobileTagList";
+export {default as TagFilters} from "./TagFilters";
